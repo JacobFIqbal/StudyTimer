f2929644c2858900f28dedb46e7720bc300bea63
JacobFI <jacobiqbal2003@gmail.com>
2023-02-22 22:00:04 +0100

    Changed a whole buch of the script

--- script.js
+++ script.js
@@ -1,34 +1,55 @@
 var countDownDate;
 var duration = document.getElementById("duration").value;
+var timeRemaining;
 var x;
 
 function setTimer() {
 	duration = document.getElementById("duration").value;
-	countDownDate = new Date();
-	countDownDate.setMinutes(countDownDate.getMinutes() + parseInt(duration));
-	resetTimer();
+	if (countDownDate) {
+		var now = new Date().getTime();
+		timeRemaining = countDownDate - now;
+		countDownDate = new Date(now + timeRemaining);
+	} else {
+		countDownDate = new Date();
+		countDownDate.setMinutes(countDownDate.getMinutes() + parseInt(duration));
+		resetTimer();
+	}
 }
 
 function startTimer() {
-	countDownDate = new Date();
-	countDownDate.setMinutes(countDownDate.getMinutes() + parseInt(duration));
+	if (!countDownDate) {
+		countDownDate = new Date();
+		countDownDate.setMinutes(countDownDate.getMinutes() + parseInt(duration));
+		resetTimer();
+	} else {
+		countDownDate = new Date();
+		countDownDate.setTime(countDownDate.getTime() + timeRemaining);
+	}
+
 	x = setInterval(function() {
 	  var now = new Date().getTime();
-	  var distance = countDownDate - now;
-	  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-	  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-	  document.getElementById("timer").innerHTML = minutes + "m " + seconds + "s ";
-	  if (distance < 0) {
+	  timeRemaining = countDownDate - now;
+	  var minutes = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
+	  var seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000);
+	  document.getElementById("timer").innerHTML = minutes + " minutes " + seconds + " seconds ";
+	  if (timeRemaining < 0) {
 	    clearInterval(x);
 	    document.getElementById("timer").innerHTML = "EXPIRED";
 	  }
 	}, 1000);
 }
 
-function resetTimer() {
+function stopTimer() {
 	clearInterval(x);
-	document.getElementById("timer").innerHTML = "0m 0s";
+	timeRemaining = countDownDate - new Date().getTime();
 }
 
+function resetTimer() {
+	clearInterval(x);
+	document.getElementById("timer").innerHTML = "Starting";
+}
+
+
+
 document.getElementById("start-btn").addEventListener("click", startTimer);
-document.getElementById("reset-btn").addEventListener("click", resetTimer);
+document.getElementById("stop-btn").addEventListener("click", stopTimer);
